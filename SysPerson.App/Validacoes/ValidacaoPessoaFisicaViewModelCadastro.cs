7b755ee6e4e8e3529590021d615a9bda686e58f2
leonardoazils <leonardo.zils@hotmail.com>
2022-04-07 06:21:33 -0300

    Ajustes nas validações de formulário.

--- SysPerson.App/Validacoes/ValidacaoPessoaFisicaViewModelCadastro.cs
+++ SysPerson.App/Validacoes/ValidacaoPessoaFisicaViewModelCadastro.cs
@@ -1,5 +1,6 @@
 ﻿using FluentValidation;
 using SysPerson.App.Models.Pessoa;
+using System;
 
 namespace SysPerson.App.Validacoes
 {
@@ -7,14 +8,15 @@ namespace SysPerson.App.Validacoes
     {
         public ValidacaoPessoaFisicaViewModelCadastro()
         {
+            RuleFor(pessoa => pessoa.Nacional).NotEmpty();
             RuleFor(pessoa => pessoa.Cpf).NotEmpty();
             RuleFor(pessoa => pessoa.Nome).NotEmpty();
             RuleFor(pessoa => pessoa.EmailPrincipal).NotEmpty();
             RuleFor(pessoa => pessoa.Profissao).NotEmpty();
             RuleFor(pessoa => pessoa.TelefonePrincipal).NotEmpty();
-            RuleFor(pessoa => pessoa.TipoEmpresaId).NotNull();
+            RuleFor(pessoa => pessoa.TipoEmpresaId).NotNull().NotEqual(Guid.Empty);
             RuleFor(pessoa => pessoa.Nascimento).NotEmpty();
-            RuleFor(pessoa => pessoa.GeneroId).NotNull();
+            RuleFor(pessoa => pessoa.GeneroId).NotNull().NotEqual(Guid.Empty);
         }
     }
 }
--- SysPerson.App/Validacoes/ValidacaoPessoaJuridicaViewModelCadastro.cs
+++ SysPerson.App/Validacoes/ValidacaoPessoaJuridicaViewModelCadastro.cs
@@ -1,5 +1,6 @@
 ﻿using FluentValidation;
 using SysPerson.App.Models.Pessoa;
+using System;
 
 namespace SysPerson.App.Validacoes
 {
@@ -7,10 +8,11 @@ namespace SysPerson.App.Validacoes
     {
         public ValidacaoPessoaJuridicaViewModelCadastro()
         {
+            RuleFor(pessoa => pessoa.Nacional).NotEmpty();
             RuleFor(pessoa => pessoa.Cnpj).NotEmpty();
             RuleFor(pessoa => pessoa.RazaoSocial).NotEmpty();
-            RuleFor(pessoa => pessoa.TipoEmpresaPjId).NotNull();
-            RuleFor(pessoa => pessoa.PorteId).NotNull();
+            RuleFor(pessoa => pessoa.TipoEmpresaPjId).NotNull().NotEqual(Guid.Empty);
+            RuleFor(pessoa => pessoa.PorteId).NotNull().NotEqual(Guid.Empty);
             RuleFor(pessoa => pessoa.TelefonePrincipalEmpresa).NotEmpty();
             RuleFor(pessoa => pessoa.EmailPrincipalEmpresa).NotEmpty();
             RuleFor(pessoa => pessoa.CapitalSocial).NotEmpty().GreaterThan(0);
